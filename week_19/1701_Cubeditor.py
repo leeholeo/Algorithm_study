5f42dc9c4b9c21fc78ff230adbb094aa8f31dd30
leeho <leeho@khu.ac.kr>
2022-06-01 03:58:27 +0900

    1701_Cubeditor solved and improved

---
+++ week_19/1701_Cubeditor.py
@@ -0,0 +1,50 @@
+import sys
+import time
+import random
+import string as STR
+
+
+# string = sys.stdin.readline().rstrip()
+string = [random.choice(STR.ascii_lowercase) for _ in range(5000)]
+# string = [random.choice("12") for _ in range(5000)]
+string = ''.join(string)
+# start_time = time.time()
+# length = len(string)
+# max_length = 0
+# for sub_start_idx in range(length):
+#     pi_array = [0] * (length-sub_start_idx)
+#     source_idx = sub_start_idx + 1
+#     target_idx = 0
+#     while source_idx < length:
+#         while string[source_idx] != string[target_idx+sub_start_idx] and target_idx > 0:
+#             target_idx = pi_array[target_idx-1]
+#         if string[source_idx] == string[target_idx+sub_start_idx]:
+#             target_idx += 1
+#             max_length = max(max_length, target_idx)
+#         pi_array[source_idx-sub_start_idx] = target_idx
+#         source_idx += 1
+# end_time = time.time()
+# print(max_length)
+# print(end_time-start_time)  # 4.5초 정도 소요
+
+# dict with complicated key / nested dict
+# string = sys.stdin.readline().rstrip()
+start_time = time.time()
+length = len(string)
+min_length = 0
+max_length = length
+while min_length < max_length:
+    now_length = (max_length+min_length) // 2 + 1
+    repetition = {}
+    for start_idx in range(length-now_length+1):
+        substring = string[start_idx:start_idx+now_length]
+        if repetition.get(substring):
+            min_length = now_length
+            break
+        else:
+            repetition[substring] = True
+    else:
+        max_length = now_length - 1
+end_time = time.time()
+print(min_length)
+print(end_time-start_time)  # 0.03초대
